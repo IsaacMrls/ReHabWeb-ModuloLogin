f1e5faca4624f575f461fc8d088aa0bfac4c658f
MadManJohnSmith <alandanielsalas@gmail.com>
2026-03-18 10:55:47 -0600

    fix(routes): Arreglar bucle infinito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'landing' },
+  {
+    path: 'landing',
+    loadComponent: () =>
+      import('./screens/landing/landing').then((m) => m.LandingComponent),
+  },
   { path: '**', redirectTo: 'landing' },
 ];
